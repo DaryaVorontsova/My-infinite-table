f8269bea664c6f48a322ab349cade6caf75f3247
Darya <148378389+DaryaVorontsova@users.noreply.github.com>
2025-06-01 22:29:37 +0300

    Add main structure and base files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { TableBlock } from './features/tableBlock/tableBlock';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <TableBlock />
+    </>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
